76e86b10bca7c24f2b0f09f963afd46ce1bbeae1
GOKUISJOD <ravikant10028@gmail.com>
2024-06-23 22:50:05 +0530

    Create firebaseConfig.js

---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyC9pNojWrccfgHeSx566J4iYo1cTMoqXvM",
+  authDomain: "goku-9e0bb.firebaseapp.com",
+  projectId: "goku-9e0bb",
+  storageBucket: "goku-9e0bb.appspot.com",
+  messagingSenderId: "869202728587",
+  appId: "1:869202728587:web:5db730499ea6eeeed5668e",
+  measurementId: "G-F39H8FFMR5"
+};
